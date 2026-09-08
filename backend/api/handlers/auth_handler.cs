8cd8c3bf30ba67dd6d217001a593047e354f5707
Angadgill98 <angadgill98@gmail.com>
2026-09-08 22:17:30 +0530

    .

--- backend/api/handlers/auth_handler.cs
+++ backend/api/handlers/auth_handler.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using backend.api.repo;
 using backend.api.services;
 using backend.Database;
+using Microsoft.AspNetCore.Identity;
 
 namespace backend.api.handlers;
 
@@ -17,12 +18,36 @@ public class AuthHandler{
         this.repos=repos;
     }
 
-    public void SignIn(Postgres_Context db,string mail,string pass)
+    public async Task SignIn(Postgres_Context db,string mail,string pass)
     {
-        
+        var user=await this.repos.auth_repo.GetUserByMail(db,mail);
+        if (user!=null)
+        {
+            Console.WriteLine("User dosent exist");
+            return;
+        }
+
+        var hasher = new PasswordHasher<User>();
+
+        var result = hasher.VerifyHashedPassword(
+            user!,
+            user!.user_pass,
+            pass
+        );
+
+        if (result == PasswordVerificationResult.Success)
+        {
+            
+            return;
+        }
+        else
+        {
+            
+            return;
+        }
     }
 
-    public async Task<(bool,Exception?)> SignUp(Postgres_Context db,string mail,string pass,string name)
+    public async Task SignUp(Postgres_Context db,string mail,string pass,string name)
     {
         var (is_saved,err)=await this.repos.auth_repo.InsertUser(name,mail,pass,db);
 
@@ -30,12 +55,11 @@ public class AuthHandler{
         {
             Console.WriteLine($"Server:failed to insert the user: \n{err}");
         
-            return (is_saved,err);
-
+            return;
         }
 
 
 
-        return (is_saved,err);
+        return;
     }
 }
--- backend/api/repo/auth.cs
+++ backend/api/repo/auth.cs
@@ -2,6 +2,7 @@
 
 
 using backend.Database;
+using Microsoft.AspNetCore.Identity;
 
 public class Auth_repo
 {
@@ -14,12 +15,16 @@ public class Auth_repo
     {
         try
         {
+
+            var hasher = new PasswordHasher<User>();
             User user=new();
+
+            string hashedPassword = hasher.HashPassword(user, pass);
 
             user.user_id=Guid.NewGuid();
             user.user_name=name;
             user.user_mail=mail;
-            user.user_pass=pass;
+            user.user_pass=hashedPassword;
 
             db.UsersTable.Add(user);
 
@@ -32,5 +37,19 @@ public class Auth_repo
             return (false,err);
         }
         
+    }
+
+    public async Task<User?> GetUserByMail(Postgres_Context db,string mail)
+    {
+        try
+        {
+            User? user=db.UsersTable.FirstOrDefault(user=>user.user_mail==mail);
+            return user;
+        }
+        catch (System.Exception)
+        {
+            
+            throw;
+        }
     }
 }
